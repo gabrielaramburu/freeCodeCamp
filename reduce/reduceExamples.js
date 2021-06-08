ed9199350df4b01e874a279c3fbb3ac4a74621d4
gabrielaramburu <gabrielaramburu@gmail.com>
2021-06-07 23:13:38 -0300

    reduce excercises

---
+++ reduce/reduceExamples.js
@@ -0,0 +1,14 @@
+const people =  [
+    {id:"1",name:"Leigh",age="35"},
+    {id:"2",name:"Jenny",age="30"},
+    {id:"3",name:"Heatear",age="28"},
+]
+
+//count people
+//sum ages
+//array of names 
+//max ages
+//find by names
+//all over 18
+//any over 18
+    //count ocurrencies
